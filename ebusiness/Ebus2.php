a8bd16008d81bd8a544ac97f6315aeb06a34d167
Ben Blackwell <117336046@umail.ucc.ie>
2018-02-09 20:58:46 +0000

    adding browsed tab icons

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Enter Details</title>
-        
+        <link rel="stylesheet" href="../mystylesheet.css" type="text/css"/>
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
